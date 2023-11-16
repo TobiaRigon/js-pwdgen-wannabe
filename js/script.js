60a9070d9e5a61600bbc7b0cbfd84490c3bacc03
TobiaRigon <rigontobia@gmail.com>
2023-11-16 15:23:39 +0100

    start to end

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Chiedi all’utente il suo nome
+const userName = prompt("Il tuo nome");
+
+
+// chiedi il suo cognome
+const userSurname = prompt("Il tuo cognome");
+
+
+// chiedi il suo colore preferito
+const userColor = prompt("Il tuo colore preferito");
+
+
+// nomecognomecolorepreferito23
+document.getElementById('my_password').innerHTML="La tua nuova password è : " + userName + userSurname + userColor + "23";
